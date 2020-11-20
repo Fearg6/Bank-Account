0314952ed07615944fa0f30c920668117e694e77
Fearghal McMorrow <fearghal.mcmorrow@gmail.com>
2020-11-20 20:40:32 +0000

    Implementing the Checking Account Debit Card

--- src/com/mcmorrow/bank/app/Checking.java
+++ src/com/mcmorrow/bank/app/Checking.java
@@ -1,19 +1,32 @@
 package com.mcmorrow.bank.app;
 
 public class Checking extends Account {
-    int debitCardNumber;
+    long debitCardNumber;
     int debitCardPIN;
 
 
     public Checking(String name, String sSN, int initDeposit) {
         super(name,sSN,initDeposit);
         accountNumber = "2"+accountNumber;
+        setDebitCard();
 
+    }
+
+    private void setDebitCard() {
+        while (debitCardNumber<100000000000L) {
+            debitCardNumber = (long) (Math.random()*Math.pow(10,12));
+        }
+        while (debitCardPIN<1000) {
+            debitCardPIN = (int) (Math.random()*Math.pow(10,4));
+        }
 
     }
 
     public void showInfo(){
         super.showInfo();
-        System.out.println("Account Type: Checking");
+        System.out.println("Account Type: Checking Account Features" +
+                "\n Debit Card Number: "+debitCardNumber+
+                "\n Debit Card PIN: "+debitCardPIN
+        );
     }
 }
