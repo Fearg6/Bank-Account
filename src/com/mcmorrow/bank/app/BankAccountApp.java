379c608cc5aaba0f4618705a423868b68fed0685
Fearghal McMorrow <fearghal.mcmorrow@gmail.com>
2021-03-14 17:41:02 +0000

    Writing a CSV Reader Utility

--- src/com/mcmorrow/bank/app/BankAccountApp.java
+++ src/com/mcmorrow/bank/app/BankAccountApp.java
@@ -1,19 +1,32 @@
 package com.mcmorrow.bank.app;
 
+import java.util.List;
+
 public class BankAccountApp {
 
     public static void main(String[] args) {
-        Checking check1 = new Checking("Tommy Wilson","1204857343",1500);
-        Savings sav1 = new Savings("Richey Love","14523575432",2500);
+        String file = "C:\\Users\\fearg\\OneDrive\\Projects\\Java\\Bank-Account\\src\\utilities\\NewBankAccounts.csv";
 
 
-        sav1.compound();
+//        Checking check1 = new Checking("Tommy Wilson","1204857343",1500);
+//        Savings sav1 = new Savings("Richey Love","14523575432",2500);
+//
+//
+//        sav1.compound();
+//
+//        check1.showInfo();
+//        System.out.println("******************");
+//        sav1.showInfo();
 
-        check1.showInfo();
-        System.out.println("******************");
-        sav1.showInfo();
 
+        List<String[]> newAccountHolders = utilities.CSV.read(file);
+        for (String[] accountHolder : newAccountHolders){
+            System.out.println("New Account:");
+            System.out.println(accountHolder[0]);
+            System.out.println(accountHolder[1]);
+            System.out.println(accountHolder[2]);
+            System.out.println(accountHolder[3]);
 
-
+        }
     }
 }
