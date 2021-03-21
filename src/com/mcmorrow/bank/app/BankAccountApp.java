c98590e12284ef91fe691e0aab43fa0acd0d2e8f
Fearghal McMorrow <fearghal.mcmorrow@gmail.com>
2021-03-21 19:50:23 +0000

    Determining Which New Account to Open

--- src/com/mcmorrow/bank/app/BankAccountApp.java
+++ src/com/mcmorrow/bank/app/BankAccountApp.java
@@ -21,12 +21,20 @@ public class BankAccountApp {
 
         List<String[]> newAccountHolders = utilities.CSV.read(file);
         for (String[] accountHolder : newAccountHolders){
-            System.out.println("New Account:");
-            System.out.println(accountHolder[0]);
-            System.out.println(accountHolder[1]);
-            System.out.println(accountHolder[2]);
-            System.out.println(accountHolder[3]);
+//            System.out.println("New Account:");
+            String name = accountHolder[0];
+            String sSn = accountHolder[1];
+            String accountType = accountHolder[2];
+            double initDeposit = Double.parseDouble(accountHolder[3]);
 
+            System.out.println(name+" "+sSn+" "+accountType+" €"+initDeposit);
+            if(accountType.equals("Savings")){
+                System.out.println("Open a Savings Account");
+            }else if(accountType.equals("Checking")){
+                System.out.println("Open a Checking Account");
+            }else {
+                System.out.println("Error Reading Account Type");
+            }
         }
     }
 }
